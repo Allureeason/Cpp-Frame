1b35c87ef43c914b39e7e0207da290b58891f51a
Allureeason <1324789783qq.com>
2022-03-24 10:04:09 +0800

    update

--- generate.sh
+++ generate.sh
@@ -19,7 +19,7 @@ namespace=$2
 
 command_error_exit mkdir $project_name
 command_error_exit cd $project_name
-command_error_exit git clone https://github.com/Allureeason/Cpp-Frame.git
+command_error_exit git clone https://github.com/Allureeason/Cpp-Frame.git sylar
 command_error_exit cp sylar/Makefile .
 command_error_exit cp -rf sylar/template/* .
 command_error_exit cp -rf sylar/template/* .
